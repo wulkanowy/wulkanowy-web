71c7244a3152604429578f7eb15d9ba1af0e62d5
Zaptyp <43276401+Zaptyp@users.noreply.github.com>
2022-04-08 21:12:58 +0200

    Simple backend test

---
+++ backend/test.py
@@ -0,0 +1,21 @@
+from fastapi.testclient import TestClient
+import json
+from main import app
+
+client = TestClient(app)
+
+
+#def test_read_item():
+    #response = client.post("/login", headers={"Content-Type": "application/json"})
+    #assert response.status_code == 405
+    #assert response.json() == {
+        #"id": "foo",
+        #"title": "Foo",
+        #"description": "There goes my hero",
+    #}
+
+def test_create_item():
+    response = client.post("/login",headers={"Content-Type": "application/json"},json={"username": "jan@fakelog.cf", "password": "jan123", "host": "fakelog.cf", "symbol": "powiatwulkanowy", "ssl": "false"},)
+    assert response.status_code == 200
+    assert response.json()["symbol"] == "powiatwulkanowy"
+    assert response.json()["host"] == "fakelog.cf"
