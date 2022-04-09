0e941c5dbc7c1e1b7a5fd2887330eac79f61fa62
Zaptyp <43276401+Zaptyp@users.noreply.github.com>
2022-04-09 09:06:45 +0200

    Add few test

--- backend/test.py
+++ backend/test.py
@@ -14,8 +14,41 @@ client = TestClient(app)
         #"description": "There goes my hero",
     #}
 
-def test_create_item():
+def test_login_correct():
     response = client.post("/login",headers={"Content-Type": "application/json"},json={"username": "jan@fakelog.cf", "password": "jan123", "host": "fakelog.cf", "symbol": "powiatwulkanowy", "ssl": "false"},)
     assert response.status_code == 200
     assert response.json()["symbol"] == "powiatwulkanowy"
     assert response.json()["host"] == "fakelog.cf"
+    ciastka = response.json()["vulcan_cookies"]
+    #print(ciastka)
+
+def test_login_incorrect():
+    response = client.post("/login",headers={"Content-Type": "application/json"},json={"username": "cipanowie@pocz.pl", "password": "dupa123", "host": "fakelog.cf", "symbol": "powiatwulkanowy", "ssl": "false"},)
+    assert response.status_code == 403
+    print(response.json())
+    assert response.json() == {
+        "detail": "Username or password is incorrect"
+    }
+    #assert response.html()["host"] == "fakelog.cf"
+    #ciastka = response.json()["vulcan_cookies"]
+    #print(ciastka)
+
+def test_symbol_incorrect():
+    response = client.post("/login",headers={"Content-Type": "application/json"},json={"username": "jan@fakelog.cf", "password": "jan123", "host": "fakelog.cf", "symbol": "warszawa", "ssl": "false"},)
+    assert response.status_code == 403
+    print(response.json())
+    assert response.json() == {
+        'detail': 'Symbol is incorrect'
+    }
+
+#def test_email_not_assigned():
+    #response = client.post("/login",headers={"Content-Type": "application/json"},json={"username": "vulcantestowy@interia.pl", "password": "Marioneq1234", "host": "vulcan.net.pl", "symbol": "warszawa", "ssl": "false"},)
+    #assert response.status_code == 403
+    #print(response.json())
+    #assert response.json() == {}
+
+
+
+
+#vulcantestowy@interia.pl
+#Marioneq1234
