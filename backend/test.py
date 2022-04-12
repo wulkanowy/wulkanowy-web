19cf926f4a74c0bd4b274c1cbdcb7f34d9b86d19
Zaptyp <43276401+Zaptyp@users.noreply.github.com>
2022-04-12 19:05:59 +0200

    Update the tests to make them easier to configure

--- backend/test.py
+++ backend/test.py
@@ -1,21 +1,32 @@
-from http import cookies
-from pydoc import cli
-from traceback import print_tb
 from fastapi.testclient import TestClient
-import json
+from requests import head
 from main import app
 
 client = TestClient(app)
-
+#Ustawienia dla wszystkich testów 
+nick = "jan@fakelog.cf"
+password = "jan123"
+host = "fakelog.cf"
+symbol = "powiatwulkanowy"
+ssl = "false"
+#Ustawienia dla test_login_incorrect i test_symbol_incorrect
+nick_invalid = "cipanowie@pocz.pl"
+password_invalid = "dupa123"
+symbol_invalid = "warszawa"
 def test_login_correct():
-    response = client.post("/login",headers={"Content-Type": "application/json"},json={"username": "jan@fakelog.cf", "password": "jan123", "host": "fakelog.cf", "symbol": "powiatwulkanowy", "ssl": "false"},)
-    assert response.status_code == 200
-    #print(response.json())
-    assert response.json()["symbol"] == "powiatwulkanowy"
-    assert response.json()["host"] == "fakelog.cf"
+    login = client.post("/login",headers={"Content-Type": "application/json"},json={"username": nick, "password": password, "host": host, "symbol": symbol, "ssl": ssl},)
+    global cookies, headars, student, school_id
+    cookies = login.json()["vulcan_cookies"]
+    headars = login.json()["students"][0]["headers"]
+    student = login.json()["students"][0]["cookies"]
+    school_id = login.json()["students"][0]["school_id"]
+    assert login.status_code == 200
+    #print(login.json())
+    assert login.json()["symbol"] == "powiatwulkanowy"
+    assert login.json()["host"] == "fakelog.cf"
 
 def test_login_incorrect():
-    response = client.post("/login",headers={"Content-Type": "application/json"},json={"username": "cipanowie@pocz.pl", "password": "dupa123", "host": "fakelog.cf", "symbol": "powiatwulkanowy", "ssl": "false"},)
+    response = client.post("/login",headers={"Content-Type": "application/json"},json={"username": nick_invalid, "password": password_invalid, "host": host, "symbol": symbol, "ssl": ssl},)
     assert response.status_code == 403
     #print(response.json())
     assert response.json() == {
@@ -23,7 +34,7 @@ def test_login_incorrect():
     }
 
 def test_symbol_incorrect():
-    response = client.post("/login",headers={"Content-Type": "application/json"},json={"username": "jan@fakelog.cf", "password": "jan123", "host": "fakelog.cf", "symbol": "warszawa", "ssl": "false"},)
+    response = client.post("/login",headers={"Content-Type": "application/json"},json={"username": nick, "password": password, "host": host, "symbol": symbol_invalid, "ssl": ssl},)
     assert response.status_code == 403
     #print(response.json())
     assert response.json() == {
@@ -31,18 +42,14 @@ def test_symbol_incorrect():
     }
 
 def test_notes():
-    response = client.post("/login",headers={"Content-Type": "application/json"},json={"username": "jan@fakelog.cf", "password": "jan123", "host": "fakelog.cf", "symbol": "powiatwulkanowy", "ssl": "false"},)
-    cookies = response.json()["vulcan_cookies"]
-    response = client.post("/uonetplus-uczen/notes",headers={"Content-Type": "application/json"},json={"vulcan_cookies": cookies, "student": {"idBiezacyDziennik": "15", "idBiezacyUczen": "1", "idBiezacyDziennikPrzedszkole": "0", "biezacyRokSzkolny": "2018"}, "school_id": "123456", "host": "fakelog.cf", "symbol": "powiatwulkanowy", "ssl": "false"},)
+    response = client.post("/uonetplus-uczen/notes",headers={"Content-Type": "application/json"},json={"vulcan_cookies": cookies, "student": student, "school_id": school_id, "host": host, "symbol": symbol, "ssl": ssl},)
     assert response.status_code == 200
     #print(response.json())
     assert response.json()['notes'][0]['teacher'] == 'Karolina Kowalska [AN]'
     assert response.json()['notes'][3]['content'] == 'Litwo! Ojczyzno moja! Ty jesteś jak zdrowie. Ile cię trzeba cenić, ten tylko aż kędy pieprz rośnie gdzie podział się? szukać prawodawstwa.'
 
 def test_grades():
-    response = client.post("/login",headers={"Content-Type": "application/json"},json={"username": "jan@fakelog.cf", "password": "jan123", "host": "fakelog.cf", "symbol": "powiatwulkanowy", "ssl": "false"},)
-    cookies = response.json()["vulcan_cookies"]
-    response = client.post("/uonetplus-uczen/grades",headers={"Content-Type": "application/json"},json={"vulcan_cookies": cookies, "student": {"idBiezacyDziennik": "15", "idBiezacyUczen": "1", "idBiezacyDziennikPrzedszkole": "0", "biezacyRokSzkolny": "2018"}, "school_id": "123456", "host": "fakelog.cf", "symbol": "powiatwulkanowy", "ssl": "false", "json": {"okres": 16}},)
+    response = client.post("/uonetplus-uczen/grades",headers={"Content-Type": "application/json"},json={"vulcan_cookies": cookies, "student": student, "school_id": school_id, "host": host, "symbol": symbol, "ssl": ssl, "json": {"okres": 16}},)
     assert response.status_code == 200
     #print(response.json())
     assert response.json()["subjects"][0]["grades"][0]["teacher"] == 'Karolina Kowalska'
@@ -50,37 +57,27 @@ def test_grades():
     #assert response.json()['grades'][3]['grade'] == '4'
     
 def test_school_info():
-    response = client.post("/login",headers={"Content-Type": "application/json"},json={"username": "jan@fakelog.cf", "password": "jan123", "host": "fakelog.cf", "symbol": "powiatwulkanowy", "ssl": "false"},)
-    cookies = response.json()["vulcan_cookies"]
-    response = client.post("/uonetplus-uczen/school-info",headers={"Content-Type": "application/json"},json={"vulcan_cookies": cookies, "student": {"idBiezacyDziennik": "15", "idBiezacyUczen": "1", "idBiezacyDziennikPrzedszkole": "0", "biezacyRokSzkolny": "2018"}, "school_id": "123456", "host": "fakelog.cf", "symbol": "powiatwulkanowy", "ssl": "false"},)
+    response = client.post("/uonetplus-uczen/school-info",headers={"Content-Type": "application/json"},json={"vulcan_cookies": cookies, "student": student, "school_id": school_id, "host": host, "symbol": symbol, "ssl": ssl},)
     assert response.status_code == 200
     #print(response.json())
 
 def test_conference():
-    response = client.post("/login",headers={"Content-Type": "application/json"},json={"username": "jan@fakelog.cf", "password": "jan123", "host": "fakelog.cf", "symbol": "powiatwulkanowy", "ssl": "false"},)
-    cookies = response.json()["vulcan_cookies"]
-    response = client.post("/uonetplus-uczen/conferences",headers={"Content-Type": "application/json"},json={"vulcan_cookies": cookies, "student": {"idBiezacyDziennik": "15", "idBiezacyUczen": "1", "idBiezacyDziennikPrzedszkole": "0", "biezacyRokSzkolny": "2018"}, "school_id": "123456", "host": "fakelog.cf", "symbol": "powiatwulkanowy", "ssl": "false"},)
+    response = client.post("/uonetplus-uczen/conferences",headers={"Content-Type": "application/json"},json={"vulcan_cookies": cookies, "student": student, "school_id": school_id, "host": host, "symbol": symbol, "ssl": ssl},)
     assert response.status_code == 200
     #print(response.json())
 
 def test_mobile_access_registed():
-    response = client.post("/login",headers={"Content-Type": "application/json"},json={"username": "jan@fakelog.cf", "password": "jan123", "host": "fakelog.cf", "symbol": "powiatwulkanowy", "ssl": "false"},)
-    cookies = response.json()["vulcan_cookies"]
-    response = client.post("/uonetplus-uczen/mobile-access/get-registered-devices",headers={"Content-Type": "application/json"},json={"vulcan_cookies": cookies, "student": {"idBiezacyDziennik": "15", "idBiezacyUczen": "1", "idBiezacyDziennikPrzedszkole": "0", "biezacyRokSzkolny": "2018"}, "school_id": "123456", "host": "fakelog.cf", "symbol": "powiatwulkanowy", "ssl": "false"},)
+    response = client.post("/uonetplus-uczen/mobile-access/get-registered-devices",headers={"Content-Type": "application/json"},json={"vulcan_cookies": cookies, "student": student, "school_id": school_id, "host": host, "symbol": symbol, "ssl": ssl},)
     assert response.status_code == 200
     #print(response.json())
 
 def test_mobile_access_register():
-    response = client.post("/login",headers={"Content-Type": "application/json"},json={"username": "jan@fakelog.cf", "password": "jan123", "host": "fakelog.cf", "symbol": "powiatwulkanowy", "ssl": "false"},)
-    cookies = response.json()["vulcan_cookies"]
-    response = client.post("/uonetplus-uczen/mobile-access/register-device",headers={"Content-Type": "application/json"},json={"vulcan_cookies": cookies, "student": {"idBiezacyDziennik": "15", "idBiezacyUczen": "1", "idBiezacyDziennikPrzedszkole": "0", "biezacyRokSzkolny": "2018"}, "school_id": "123456", "host": "fakelog.cf", "symbol": "powiatwulkanowy", "ssl": "false"},)
+    response = client.post("/uonetplus-uczen/mobile-access/register-device",headers={"Content-Type": "application/json"},json={"vulcan_cookies": cookies, "student": student, "school_id": school_id, "host": host, "symbol": symbol, "ssl": ssl},)
     assert response.status_code == 200
     #print(response.json())
 
 def test_mobile_access_delete_registed():
-    response = client.post("/login",headers={"Content-Type": "application/json"},json={"username": "jan@fakelog.cf", "password": "jan123", "host": "fakelog.cf", "symbol": "powiatwulkanowy", "ssl": "false"},)
-    cookies = response.json()["vulcan_cookies"]
-    response = client.post("/uonetplus-uczen/mobile-access/delete-registered-device",headers={"Content-Type": "application/json"},json={"vulcan_cookies": cookies, "student": {"idBiezacyDziennik": "15", "idBiezacyUczen": "1", "idBiezacyDziennikPrzedszkole": "0", "biezacyRokSzkolny": "2018"}, "school_id": "123456", "host": "fakelog.cf", "symbol": "powiatwulkanowy", "ssl": "false", "json": {"id": 1}, "headers": {},})
+    response = client.post("/uonetplus-uczen/mobile-access/delete-registered-device",headers={"Content-Type": "application/json"},json={"vulcan_cookies": cookies, "student": student, "school_id": school_id, "host": host, "symbol": symbol, "ssl": ssl, "json": {"id": 1}, "headers": headars,})
     #Nowa metoda testowania
     #if response.status_code == 404:
     #    print(response.json())
