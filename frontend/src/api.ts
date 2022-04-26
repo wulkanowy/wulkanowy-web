2ad23fa6e402be21811dbc433b3e1b888fa4e75b
Marioneq 4958 <mq4958@gmail.com>
2022-04-26 17:51:28 +0200

    Change api.ts requested by MRmlik12

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,36 +1,26 @@
-import axios, { AxiosResponse } from "axios";
-import store from "@/store/index";
+import axios from 'axios';
+import store from '@/store/index';
 
 export default {
   login: async (username: string, password: string, symbol: string, host: string, ssl: boolean) => {
-    try {
-      const response = await axios({
-        method: "POST",
-        url: "http://localhost:8000/login",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        data: {
-          username,
-          password,
-          symbol,
-          host,
-          ssl,
-        },
-        withCredentials: true,
-      });
-      if (!response.data.students.length) {
-        store.state.error.description = "This account have not any students";
-        store.state.error.show = true;
-        store.state.loading = false;
-      } else {
-        store.state.logged_in = true;
-        store.state.loading = false;
-        return response;
-      }
-    } catch (error: any) {
-      if (error.toJSON().message == "Network Error") {
-        store.state.error.description = "No internet connection";
+    const response: any = await axios({
+      method: 'POST',
+      url: 'http://localhost:8000/login',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: {
+        username,
+        password,
+        symbol,
+        host,
+        ssl,
+      },
+      withCredentials: true,
+    })
+    .catch(function (error: any) {
+      if(error.toJSON().message == 'Network Error'){
+        store.state.error.description = 'No internet connection';
         store.state.error.details = error.toJSON().stack;
         store.state.error.show = true;
         store.state.loading = false;
@@ -40,6 +30,16 @@ export default {
         store.state.error.show = true;
         store.state.loading = false;
       }
+    });
+    if (!response.data.students.length) {
+      store.state.error.description = 'This account have not any students';
+      store.state.error.show = true;
+      store.state.loading = false;
+      return null;
+    } else {
+      store.state.logged_in = true;
+      store.state.loading = false;
+      return response;
     }
   },
 };
